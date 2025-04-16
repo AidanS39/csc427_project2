829da248c8f7f40ef88284239ba6e6c5c7abffe7
AidanS39 <aidan.stoner39@gmail.com>
2025-04-16 17:46:43 -0400

    refactored t1.py

--- project2/t1.py
+++ project2/t1.py
@@ -1,26 +1,21 @@
-def unigram(corpus, word):
+def compute_unigram(corpus, word):
     return corpus.count(word) / len(corpus)
 
-def unigram_model(corpus):
-    unigram_model = {w : unigram(corpus, w) for w in corpus}
-    return unigram_model
+def compute_unigram_model(corpus):
+    return {w : compute_unigram(corpus, w) for w in corpus}
 
-def Bigram_MLE(list, unigram):
-    bigrams = {(list[i], list[i+1]): sum(1 for j in range(len(list) - 1) if list[j] == list[i] and list[j+1] == list[i+1]) / (unigram[list[i]] * len(list)) for i in range(len(list) - 1)}
-    return bigrams
-
+def compute_bigram(corpus, w1, w2):
+    return sum(1 for i in range(len(corpus) - 1) if corpus[i] == w1 and corpus[i+1] == w2)  / (corpus.count(w1)) 
     
-                
-
-
-
+def compute_bigram_model(corpus):
+    return {(w1, w2): compute_bigram(corpus, w1, w2) for w2 in set(corpus) for w1 in set(corpus)} 
 
 words = []
 
 with open('corpus.txt', "r") as file:
     words = file.read().split()
 
-unigrams = unigram_model(words)
-print(unigrams)
-bigrams = Bigram_MLE(words, unigrams)
+unigrams = compute_unigram_model(words)
+# print(len(words))
+bigrams = compute_bigram_model(words)
 print(bigrams)
