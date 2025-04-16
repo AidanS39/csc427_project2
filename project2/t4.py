eaa2adade4ed844216cbfaa8d7ec0c02331b7520
AidanS39 <aidan.stoner39@gmail.com>
2025-04-16 18:50:29 -0400

    added function to compute bigram PPL

--- project2/t4.py
+++ project2/t4.py
@@ -1,5 +1,31 @@
-import math
+def compute_unigram(corpus, word):
+    return corpus.count(word) / len(corpus)
+
+def compute_unigram_model(corpus):
+    return {w : compute_unigram(corpus, w) for w in corpus}
+
+def compute_bigram(corpus, w1, w2):
+    return sum(1 for i in range(len(corpus) - 1) if corpus[i] == w1 and corpus[i+1] == w2)  / (corpus.count(w1)) 
+    
+def compute_bigram_model(corpus):
+    return {(w1, w2): compute_bigram(corpus, w1, w2) for w2 in set(corpus) for w1 in set(corpus)} 
 
 # computes the bigram perplexity of a test set
 def compute_bigram_ppl(test_set, model):
-    return pow(math.prod([model[test_set[i]][test_set[i-1]] for i in range(1, len(test_set)]), len(test_set))
+    total = 1 
+    for t in [1 / model[test_set[i], test_set[i+1]] for i in range(len(test_set) - 1)]:
+        total *= t
+    return pow(total, 1 / len(test_set))
+
+words = []
+
+with open('corpus.txt', "r") as file:
+    words = file.read().split()
+
+unigrams = compute_unigram_model(words)
+# print(len(words))
+bigrams = compute_bigram_model(words)
+print(bigrams)
+
+bigram_perplexity = compute_bigram_ppl(words, bigrams)
+print(bigram_perplexity)
