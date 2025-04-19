d8180a3359a43418d052538d893e8651ae1aae37
AidanS39 <aidan.stoner39@gmail.com>
2025-04-18 20:33:54 -0400

    removed t1-t3 (we only are submitting 1 source code deliverable), and made some minor naming and stylistic changes

--- project2/d1.py
+++ project2/d1.py
@@ -32,25 +32,43 @@ def compute_bigram_smoothed(corpus, w1, w2):
 def compute_bigram_smoothed_model(corpus):
     return {(w1, w2): compute_bigram_smoothed(corpus, w1, w2) for w2 in set(corpus) for w1 in set(corpus)} 
 
+# Generate Unigram Sentence
+
+def generate_sentence_unigram(model):
+    # TODO: write function to generate sentence using a unigram model!
+
 # Generate Bigram Sentence
 
-def generate_sentence_bigram(bigram):
+def generate_sentence_bigram(model):
     sentence = ["<s>"]
     index = 1
     while (sentence[index-1] != "</s>"):
-        number = random.random()
-        print(number)
-        count = 0
-        for pair in bigram:
-            if (pair[0] == sentence[index-1] and bigram.get(pair) != 0.0):
+        random_prob = random.random()
+        print(random_prob)
+        sum_prob = 0
+        for pair in model:
+            if (pair[0] == sentence[index-1] and model.get(pair) != 0.0):
                 print(pair)
-                count += bigram.get(pair)
-                if count >= number:
+                sum_prob += model.get(pair)
+                if sum_prob >= random_prob:
                     sentence.append(pair[1])
                     index += 1
                     break
         print(sentence)
         print(index)
+
+# Unigram Perplexity
+
+def compute_unigram_ppl(test_set, model):
+    # TODO: write function to compute the perplexity of a test set using a unigram model! (is this needed?)
+
+# Bigram Perplexity
+
+def compute_bigram_ppl(test_set, model):
+    total = 1 
+    for t in [1 / model[test_set[i], test_set[i+1]] for i in range(len(test_set) - 1)]:
+        total *= t
+    return pow(total, 1 / len(test_set))
 
 # Examples
 
@@ -67,7 +85,9 @@ bigrams = compute_bigram_model(words)
 smoothed_unigrams = compute_unigram_smoothed_model(words)
 # print(smoothed_unigrams)
 smoothed_bigrams = compute_bigram_smoothed_model(words)
-print(smoothed_bigrams)
+# print(smoothed_bigrams)
 
+generate_sentence_bigram(bigrams)
 
-# generate_sentence_bigram(bigrams)
+bigram_perplexity = compute_bigram_ppl(words, bigrams)
+# print(bigram_perplexity)
